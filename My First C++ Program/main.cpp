377713a9d80af8583baf2ccd4c91e3a42a2ebee8
wobetu <wobetushiferaw@gmail.com>
2019-11-22 01:11:46 -0800

    My First C++ Program written here

---
+++ My First C++ Program/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// This is my first C++ program
+using namespace std;
+
+int main()
+{
+    // The following lines print a hello world message.
+    cout << "Hello world! I am C++ Program."<<endl;
+    cout<<"This is my first C++ program.\n";
+    cout<<"This is another C++ statement.";
+    return 0;
+}
---
+++ TestC++/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world. I am C++";
+    return 0;
+}
